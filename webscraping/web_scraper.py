2715aefe00486b646ad46bcd8be992922308c8d0
Taekwan Yoon <taekwan@ualberta.ca>
2022-11-06 00:26:59 -0600

    optimized and added comments to the web_scraper file

--- webscraping/web_scraper.py
+++ webscraping/web_scraper.py
@@ -1,4 +1,25 @@
-# BeautifulSoup for web scraping 
+# Team Name: KANDY
+# Date: November 6, 2022
+# Project: Analyzing Information on Car Collion in Canada
+# Competition: HACKED(beta) 2022 at the University of Alberta
+# Author: Taekwan Yoon
+# Team Members: Taekwan Yoon, Min Joh, Jamie Lee, Yongbin Kim, Dohyun Kim
+
+# Description of file: 
+#   This file scrapes the website: "https://www.navbug.com/alberta_traffic.htm".
+#   The website provides data on news reports on car collisions. Although the
+#   number of news reports may not accurately reflect the actual number of 
+#   car collisions, this website provided one of the most thoroughly tracked
+#   and systematic information.  
+
+# Side note:
+#   In the same folder, the output text file was generated in Nov 6, 2022.
+#   In the terminal, type the following to newly generate the output text file:
+#               $ python3 web_scraper.py <output_file_name>
+#
+#   Warning: the compiling time may vary depending on the computer performance.
+
+# BeautifulSoup for web scraping (no JavaScript used) 
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -31,6 +52,7 @@ def num_cities(url):
 
     return list_cities
 
+
 def format_list(list_cities):
     """
     This function modifies the format of the city list so that it can be incorprated in hyperlink.
@@ -39,7 +61,7 @@ def format_list(list_cities):
     
     hyper_city_list = []
     
-    # modify the format
+    # modify the format to match the hyperlinks
     for city in list_cities:
         city = city.replace(" ", "_").lower()
         if "_traffic" in city:
@@ -59,10 +81,11 @@ def get_archived_titles(full_link):
     """
     This function returns the titles of the archived posts.
     """
-    url = full_link
-    result = requests.get(url)
+
+    result = requests.get(full_link)
     soup = BeautifulSoup(result.text, "lxml")
-    # all posts have "Incident Archives"
+    
+    # all posts have "Incident Archives" string
     post_titles = soup.find_all(string = re.compile("Incident Archives"))
     archived_post_list = []
     for title in post_titles:
@@ -71,13 +94,16 @@ def get_archived_titles(full_link):
 
 def trim_archives(archived_post_list):
     """
-    This function removes the posts that are not of concern (prior to 2021).
+    This function removes posts that are not of concern (prior to 2021).
+
+    Prior to 2021, a lot of data are missing or have been erased.
     """
+    
     list2021 = []
     list2022 = []
 
     for post in archived_post_list:
-        if "2021" in post:
+        if "2021" in post: 
             list2021.append(post)
         elif "2022" in post:
             list2022.append(post)
@@ -88,11 +114,18 @@ def trim_archives(archived_post_list):
 def get_archive_link(archive_list, year, city):
     """
     This function returns the list of links to archived reports.
+
+    It takes the post titles, year of concern (2021 or 2022),
+    and city of interest as its argument.
     """
 
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    months = [
+            "January", "February", "March", "April", "May", "June", "July", 
+            "August", "September", "October", "November", "December"
+            ]
 
     month_list = []
+    # hyperlinks use integers, instead of strings to specify months
     for title in archive_list:
         if months[0] in title:
             month_list.append("01")
@@ -119,20 +152,24 @@ def get_archive_link(archive_list, year, city):
         elif months[11] in title:
             month_list.append("12")
 
+    # hyperlinks use year + months
     date_list = []
     for i in range(len(month_list)):
         date_list.append(str(year) + month_list[i])
-
+    
+    # full hyperlinks
     link_list = []
-    for date in date_list:                              # to change the area, change "edmonton"
+    for date in date_list:
         link_list.append("https://www.navbug.com/alberta/{}/archives/{}.htm".format(city,date))
     return link_list
 
 def num_posts(link_list):
     """
-    This function returns the number of posts in one year.
+    This function returns the number of news reports in a year.
     """
+
     num_posts = 0
+    
     for link in link_list:
         result = requests.get(link)
         soup = BeautifulSoup(result.text, "lxml")
@@ -140,38 +177,78 @@ def num_posts(link_list):
         post_list = []
         for post in post_titles:
             post_list.append(post.text)
-        num_posts += len(post_list)
+        num_posts += len(post_list) 
+
     return num_posts
 
 def list_options(list_cities):
     """
-    This function shows the list of city options that you can input in "get_archived_link" function.
+    This function shows the list of city options that you can input in
+    "get_archived_link" function. The list has specific formats. 
     """
+
     list_of_cities = []
     for city in list_cities:
+        # all lowercase, whitespace --> underscore, erase "_traffic"
         list_of_cities.append(city.lower().replace(" ","_").replace("_traffic",""))
     
     return list_of_cities
 
-def scrape_main(city):
+def scrape_main(city,index):
+    """
+    This function applies many of scraping tasks performed by other functions
+    in this module.
+    """
+    
+    # main page to get list of city names
     list_cities = num_cities("https://www.navbug.com/alberta/calgary_traffic.htm")
+    
+    # modify and format city names to use for webscraping (must match hyperlinks)
     formatted_list = format_list(list_cities)
-    print(list_options(list_cities))
-    print(formatted_list)
-    archive_list = get_archived_link(formatted_list[5]) #[1] is the edmonton
+    
+    # each city has multiple links corresponding to months & years
+    # so make a list of hyperlinks corresponding to these months & years
+    archive_list = get_archived_link(formatted_list[index])
+
+    # in each link of specific month & year, get the number of news reports 
     archived_titles = get_archived_titles(archive_list)
+
+    # remove news reports before 2021 because many of them were removed and empty
     list2021, list2022 = trim_archives(archived_titles)
+
+    # scrape data for specified city in 2021
     list_of_archived_link2021 = get_archive_link(list2021, 2021, city)
+
+    # scrape data for specified city in 2022
     list_of_archived_link2022 = get_archive_link(list2022, 2022, city)
+
+    # number of news reports in 2021 in specified city
     num2021 = num_posts(list_of_archived_link2021)
+
+    # number of news reports in 2022 in specified city
     num2022 = num_posts(list_of_archived_link2022)
+
+    print(city)
     print('2021:', num2021)
     print('2022:', num2022)
 
+def main():
+    
+    # get the list of all cities
+    list_cities = num_cities("https://www.navbug.com/alberta/calgary_traffic.htm")
+    
+    # format the city for displaying output
+    formatted_list = format_list(list_cities)
+
+    # show city options in output
+    option_list = list_options(list_cities)
+    print(option_list)
+    
+    # loop through each city to get number of news reports in each year
+    for i in range(len(option_list)):
+        city = option_list[i]
+        scrape_main(city, i)
+
 if __name__ == "__main__":
-    scrape_main("medicine_hat")
-
-
-
-
+    main()
 
