629980ae98eb3ebfb0aa616a734984c7b9790642
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2022-11-06 01:56:32 -0600

    added some maps

---
+++ map_proj/src/dashboard/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import geocoder
+
+# Create your models here.
+class Data(models.Model):
+    location = models.CharField(max_length = 100, null = True)
+    collisions = models.PositiveBigIntegerField()
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Data'
+        
+    def save(self,*args,**kwargs):
+        self.latitude = geocoder.osm(self.location).lat
+        self.longitude = geocoder.osm(self.location).lng
+        return super().save(*args, **kwargs)
+
+    def update(self,*args, **kwargs):
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.location
